d1cbb42f23efddf3265bfe9899ff34d21246e32d
Theo Browne <me@theo.li>
2020-07-15 12:42:56 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const [showCounter, setShowCounter] = React.useState(true);
+  return (
+    <div className="App">
+      <h1>Demo</h1>
+      <button onClick={() => setShowCounter(!showCounter)}>
+        Click to toggle counter
+      </button>
+      {showCounter && <Counter />}
+    </div>
+  );
+}
+
+export const Counter = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      {count}
+    </div>
+  );
+};
